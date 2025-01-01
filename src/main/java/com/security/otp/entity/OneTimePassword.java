6cbb581af69a7443c2a4ba992fdb47d04fe63a89
hedi smiri <hedi.smiri@technica-engineering.de>
2025-01-01 22:06:21 +0100

    Add OneTimePassword POJO id,oneTimePasswordCode,expires fields

---
+++ src/main/java/com/security/otp/entity/OneTimePassword.java
@@ -0,0 +1,25 @@
+package com.security.otp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Entity
+public class OneTimePassword {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NonNull
+    private Long id;
+    @NonNull
+    private Integer oneTimePasswordCode;
+    @NonNull
+    private Date expires;
+}
