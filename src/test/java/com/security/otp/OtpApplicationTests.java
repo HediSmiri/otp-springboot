d61135e1096b75412f9419525f59992164c9ecbb
hedi smiri <hedi.smiri@technica-engineering.de>
2025-01-01 21:56:44 +0100

    Initialize project with Docker Compose infrastructure setup

---
+++ src/test/java/com/security/otp/OtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.otp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
